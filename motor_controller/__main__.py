2aeadfeb84d578f6da876537ded4d33ab92931dc
Kevin Brey <manbearwiz@gmail.com>
2016-08-30 11:15:51 -0500

    feat(websocket): switch to autobahn and asyncio

--- motor_controller/__main__.py
+++ motor_controller/__main__.py
@@ -1,19 +1,26 @@
 """Main module to control the fan motors in the fans of fury game."""
 
-import time
 import sys
 import signal
 import configparser
 import logging
+import json
+from datetime import datetime
 
+import asyncio
 from gpiocrust import Header, PinMode
+from autobahn.asyncio.websocket import WebSocketClientProtocol, WebSocketClientFactory
 
-from .web_socket_handler import WebSocketHandler
+from .fan_device import FanDevice
+from .fan_speed_change_event import FanSpeedChangeEvent
+from .fans_of_fury import FansOfFury
+from .device_controller_registration_event import DeviceControllerRegistrationEvent
 
-cfg = configparser.ConfigParser()
-cfg.read('config.ini')
+CONFIG = configparser.ConfigParser()
+CONFIG.read('config.ini')
 
 logging.basicConfig(level=logging.DEBUG)
+
 
 def main(args=None):
     """The main routine."""
@@ -22,27 +29,69 @@ def main(args=None):
 
     def sigterm_handler(signum, frame):
         logging.info('GAME: received sigterm')
-        socket.server_socket.close()
+        loop.close()
         sys.exit()
 
     signal.signal(signal.SIGTERM, sigterm_handler)
 
-    # Loop for REAL forever
-    logging.debug('SOCKET: Connecting to websocket server...')
-    while 1:
-        try:
-            with Header(mode=PinMode.BCM) as header:
-                with WebSocketHandler(cfg) as socket:
-                    socket.run()
-        except KeyboardInterrupt:
-            logging.info('GAME: received keyboard interrupt.')
-            socket.server_socket.close()
-            sys.exit()
-        except:
-            exception = sys.exc_info()[1]
-            logging.error(exception)
-            logging.error('SOCKET: Reconnecting...')
-            time.sleep(20)
+    class FansOfFuryClientProtocol(WebSocketClientProtocol):
+
+        def __init__(self):
+            self.fof = None
+
+        def onOpen(self):
+            logging.debug('SOCKET: Socket opened')
+
+            self.fof = FansOfFury(CONFIG)
+
+            # Register this device controller and its devices
+            registration_message = DeviceControllerRegistrationEvent(
+                self.fof.device_controller_config)
+            self.send_websocket_message(registration_message)
+
+            payload = json.dumps(registration_message, default=lambda o: o.__dict__,
+                                 sort_keys=True, ensure_ascii=False).encode('utf8')
+            logging.debug(
+                'SOCKET: Sending message through socket: {}'.format(payload))
+            self.sendMessage(payload, isBinary=False)
+
+        def onMessage(self, payload, isBinary):
+            if not isBinary:
+                #res = json.loads(payload.decode('utf8'))
+                #print("Result received: {}".format(res))
+                # self.sendClose()
+
+                logging.debug('SOCKET: Rec\'d message: ' + payload)
+                dic = json.loads(payload.decode('utf8'))
+                dic['device'] = FanDevice(**dic['device'])
+                fan_speed_change_event = FanSpeedChangeEvent(**dic)
+                logging.debug('SOCKET: New value: ' +
+                              str(fan_speed_change_event.new_speed))
+                fan_id = int(fan_speed_change_event.device.id)
+                logging.debug('Fan ID is ' + fan_speed_change_event.device.id)
+                new_value = float(fan_speed_change_event.new_speed)
+
+                self.fof.PLAY_SIDES[fan_id].motor.change_speed(new_value)
+                if new_value > self.fof.PLAY_SIDES[fan_id].motor.MIN_PERCENTAGE:
+                    self.fof.PLAY_SIDES[
+                        fan_id].motor.last_event_date = datetime.now()
+
+        def onClose(self, wasClean, code, reason):
+            if reason:
+                print(reason)
+            loop.stop()
+
+    with Header(mode=PinMode.BCM) as header:
+        factory = WebSocketClientFactory(
+            u'ws://192.168.31.99:8080/ws/fancontroller/websocket')
+        factory.protocol = FansOfFuryClientProtocol
+
+        loop = asyncio.get_event_loop()
+        coro = loop.create_connection(factory, '192.168.31.99', 8080)
+        loop.run_until_complete(coro)
+        loop.run_forever()
+
+    loop.close()
 
 if __name__ == "__main__":
     main()
